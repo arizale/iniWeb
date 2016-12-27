7f678562c1b9146d8513fb022957fb2b8287fbd0
iNcidiuZ <rizal160692@gmail.com>
2016-12-27 18:29:19 +0700

    iniPR

---
+++ assets/js/fungsiGambar.js
@@ -0,0 +1,15 @@
+function fungsiSatu(){
+document.getElementById('myImage').src='satu.png';
+}
+
+function fungsiDua(){
+document.getElementById('myImage').src='dua.png';
+}
+
+function fungsiTiga(){
+document.getElementById('myImage').src='tiga.png';
+}
+
+function fungsiEmpat(){
+document.getElementById('myImage').src='empat.png';
+}
