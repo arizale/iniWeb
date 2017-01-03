369678e97559ad4e675c960fc0c3727d0f0a7761
Unknown <yudaprsetyo@gmail.com>
2017-01-03 18:13:32 +0700

    punya aduy

---
+++ dor/fungsi.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#tombol1').click(function(){
+    $("#myImg").attr('src',"1.png");
+    });
+
+    $('#tombol2').click(function(){
+    $("#myImg").attr('src',"2.png");
+    });
+
+    $('#tombol3').click(function(){
+    $("#myImg").attr('src',"3.png");
+    });
+
+    $('#tombol4').click(function(){
+    $("#myImg").attr('src',"4.jpg");
+    });
+
+    $('#tombol5').click(function(){
+    $("#myImg").attr('src',"5.png");
+    });
+
+});
